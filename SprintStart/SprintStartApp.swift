10af518d428e8d2f346d5ad7533c8c90e5163ffb
zkralec <zkralec@icloud.com>
2025-06-10 16:50:53 -0400

    Initial Commit

---
+++ SprintStart/SprintStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintStartApp.swift
+//  SprintStart
+//
+//  Created by Zachary Kralec on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SprintStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SprintStartTests/SprintStartTests.swift
@@ -0,0 +1,16 @@
+//
+//  SprintStartTests.swift
+//  SprintStartTests
+//
+//  Created by Zachary Kralec on 6/10/25.
+//
+
+import Testing
+
+struct SprintStartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
